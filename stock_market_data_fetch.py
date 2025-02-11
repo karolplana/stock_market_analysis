ab9cbd1758f0aceeea6e8f9a540387086e19b678
karolplana <karolplana1@gmail.com>
2025-02-10 21:43:09 -0700

    Adding user input functionality and comments

--- stock_market_data_fetch.py
+++ stock_market_data_fetch.py
@@ -4,12 +4,14 @@ Assignment: Stock Market Data Analysis
 Due Date:   2/10/2025
 Version:    1.0
 AI Usage Disclaimers: Copilot and ChatGPT were heavily used to explore implementations of dictionary methods as well
-as give explanations to many of the lines of code provided. This code was also created with the help of Grant Bowers. We
-worked on developing the functions together.
+as give explanations to many of the lines of code provided. Sources such as GeeksForGeeks and W3 schools also helped
+with the understanding of how to handle API calls and resulting information. This code was also created with the help
+of Grant Bowers. We worked on developing the functions together.
 """
-import requests
-import json
 from datetime import date
+import json
+import requests
+import sys
 
 def download_data(ticker: str) -> dict:
     """
@@ -45,10 +47,14 @@ def data_processing(stocks: dict) -> dict:
     Returns:
         dict: A dictionary containing the min, max, avg, and median of the closing prices.
     """
-    #Create a list only containing the rows from the dictionary (i.e. information for each day)
-    modified_stocks = list(stocks["data"]["tradesTable"]["rows"])
-    #List to store the close values for future operations.
-    close_values = []
+    try:
+        #Create a list only containing the rows from the dictionary (i.e. information for each day)
+        modified_stocks = list(stocks["data"]["tradesTable"]["rows"])
+        #List to store the close values for future operations.
+        close_values = []
+    except Exception as error:
+        print(f"Exception occurred {error}")
+        return {}
 
     for stock in modified_stocks:
         close_price = float(stock["close"].replace('$',''))
@@ -63,18 +69,25 @@ def data_processing(stocks: dict) -> dict:
     median_close = sorted(close_values)[len(close_values) // 2]
     return {"ticker": symbol, "max_close": max_close, "min_close": min_close, "avg_close": avg_close, "median_close": median_close}
 
-"""Data Download Section"""
-tickers = ["aapl", "msft", "googl", "amzn", "tsla"]
-filtered_tickers = []
-data_path = "raw_data.json"
-filtered_path = "stocks.json"
+if __name__ == "__main__":
+    """Data Download Section"""
+    tickers = []
+    #Read in arguments from user
+    for ticker in sys.argv[1:]:
+        tickers.append(ticker)
+    filtered_tickers = []
 
-for ticker in tickers:
-    data = download_data(ticker)
-    # Write information to JSON file to verify functionality
-    with open(data_path, "w") as file:
-        json.dump(data, file, indent=4)
-        print(f"JSON data written to {data_path}")
-    filtered_tickers.append(data_processing(data))
-    with open(filtered_path, "w") as file:
-        json.dump(filtered_tickers, file, indent=4)
+    #File Paths
+    data_path = "raw_data.json"
+    filtered_path = "stocks.json"
+
+    #Run the code using the tickers in the list. Final result is placed in the stocks.json
+    for ticker in tickers:
+        data = download_data(ticker)
+        # Write information to JSON file to verify functionality
+        with open(data_path, "w") as file:
+            json.dump(data, file, indent=4)
+            print(f"JSON data written to {data_path}")
+        filtered_tickers.append(data_processing(data))
+        with open(filtered_path, "w") as file:
+            json.dump(filtered_tickers, file, indent=4)
